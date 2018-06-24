8c5e2fe4bdd82818e0479fbf639cc0edfcfa0dcc
bodems <bodems@wopr>
2018-06-24 12:53:04 +0000

    added webinterface from Freifunk Bielefeld

---
+++ files/www/lan/upgrade.js
@@ -0,0 +1,32 @@
+
+function init() {
+	tr();
+}
+
+function restore_firmware() {
+	if (!confirm(tr("tr_really_reset"))) {
+		return;
+	}
+
+	send("/cgi-bin/upgrade", { func : 'restore_firmware' }, function(text) {
+		setText('msg', text);
+	});
+}
+
+function lookup_upgrade() {
+	setText('msg', 'tr_try_server');
+	send("/cgi-bin/upgrade", { func : 'lookup_upgrade' }, function(text) {
+		setText('msg', text);
+	});
+}
+
+function lookup_and_apply_upgrade() {
+	if (!confirm(tr("tr_really_update"))) {
+		return;
+	}
+
+	setText('msg', 'tr_manual_update');
+	send("/cgi-bin/upgrade", { func : 'lookup_and_apply_upgrade' }, function(text) {
+		setText('msg', text);
+	});
+}
